901a5c12f19bb507f0127b836f841e26bfcd0cbd
Jacob <jaha@kvuc.dk>
2015-06-09 15:10:57 +0200

    Kloner snapper til dropzonen og kan slettes ved at trække dem ud af dropzonen

--- builds/development/ionforbindelser.js
+++ builds/development/ionforbindelser.js
@@ -88,34 +88,85 @@ function CreateIons(JsonObj) {
 //                        Run code....
 //########################################################################
 
+var outSide;
 
 $( document ).ready(function() {
 
-    console.log("NameArray 2: " + NameArray);
+    //console.log("NameArray 2: " + NameArray);
 
     CreateIons(JsonObj);
 
-    $( ".draggable" ).draggable({ 
-        revert: 'invalid',  // Makes the draggable revert back if does not have class "Elem_OK", "Coeff_OK", "Char_OK" or "Index_OK". 
+    $('.draggable').mousedown(function(){
+        original = true;
+    })
+
+    $(".draggable").draggable({ 
+        revert: 'invalid',  // Makes the draggable revert back if does not have class plus or minus and is dropped in the corresponding div 
         helper: 'clone',
         stop: function (e, ui) {
             $('.draggable').draggable().data()["ui-draggable"].cancelHelperRemoval = true;
         }
     });
-
-    $( ".DropMinus" ).droppable({
+    $(".DropMinus").droppable({
         accept: ".minus",
         drop: function(ev, ui) {
-            $(ui.draggable.clone()).detach().css({top: -20,left: 0, margin:0}).appendTo(this).addClass('clone');
-            // $(ui.draggable.clone()).draggable('disable');
+            if(original) {
+                $(ui.draggable.clone()).detach().css({top: -20,left: 0, margin:0}).appendTo(this).addClass('clone');
+                original = false;
+                $('.clone').draggable({
+                    revert: true,
+                })
+            }
+            console.log('outside: '+outSide);
+        },
+        out: function (event, ui) {
+            if (!original){
+                $(ui.draggable).fadeTo('fast', 0.5);
+                outSide = true;
+                $('.clone').draggable({
+                    revert: false,
+                    stop: function (e, ui) {
+                        $(this).remove();
+                    }
+                })
+            }
+        },
+        over: function (event, ui) {
+            if (!original){
+                $(ui.draggable).fadeTo('fast', 1.0);
+                outSide = false;
+            }
         }
     });
-
-    $( ".DropPlus" ).droppable({
+    $(".DropPlus").droppable({
         accept: ".plus",
         drop: function(ev, ui) {
-            $(ui.draggable.clone()).css({top: 0,left: 0, margin:0}).appendTo(this).addClass('clone');
-            // $(ui.draggable.clone()).draggable({ disabled: true });
+            if(original) {
+                $(ui.draggable.clone()).detach().css({top: 0,left: 0, margin:0}).appendTo(this).addClass('clone');
+                original = false;
+                $('.clone').draggable({
+                    revert: true,
+                })
+            }
+            console.log('outside: '+outSide);
+        },
+        out: function (event, ui) {
+            if (!original){
+                $(ui.draggable).fadeTo('fast', 0.5);
+                outSide = true;
+                $('.clone').draggable({
+                    revert: false,
+                    stop: function (e, ui) {
+                        $(this).remove();
+                    }
+                })
+            }
+        },
+        over: function (event, ui) {
+            if (!original){
+                $(ui.draggable).fadeTo('fast', 1.0);
+                outSide = false;
+            }
         }
     });
 });
